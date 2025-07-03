6a42a437b5a1e3095045847d1dec3d1f143a4a84
IzaTecbr <izapay4devs@gmail.com>
2025-07-03 19:36:53 -0300

    testando um novo commit

--- src/powerclasses/PowerDates.java
+++ src/powerclasses/PowerDates.java
@@ -5,15 +5,6 @@ import java.time.format.DateTimeFormatter;
 
 public class PowerDates {
     public static void main(String[] args) {
-        //classe que possui o papel de formatar um instante, data e hora
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        //classe que representa somente uma data
-        LocalDate agora = LocalDate.now();
-        LocalDate vencimento = LocalDate.of(2025,7,2);
-        String vencimentoBr = formatter.format(vencimento);
-        //metodo que verifica se uma data é posterior a outra
-        if(agora.isAfter(vencimento)){
-            System.out.println("Parcela vencida na data " + vencimentoBr);
-        }
+       
     }
 }
