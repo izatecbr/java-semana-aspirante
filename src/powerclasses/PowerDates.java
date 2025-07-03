c72101e2a91d5759f7f45ef0af6d506da2c9749c
IzaTecbr <izapay4devs@gmail.com>
2025-07-03 19:50:58 -0300

    classes

--- src/powerclasses/PowerDates.java
+++ src/powerclasses/PowerDates.java
@@ -1,10 +1,18 @@
 package powerclasses;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 
 public class PowerDates {
     public static void main(String[] args) {
-       
+       LocalDate hoje = LocalDate.now();
+       LocalDate primeiroVencimento=hoje.plusMonths(1);
+
+       int mesPrimeiroVencimento = primeiroVencimento.getMonthValue();
+        System.out.println(mesPrimeiroVencimento);
     }
 }
---
+++ src/powerclasses/PowerNumbers.java
@@ -0,0 +1,11 @@
+package powerclasses;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class PowerNumbers {
+    public static void main(String[] args) {
+        System.out.println("Classe destinada a aprender sobre números na linguagem");
+    }
+}
